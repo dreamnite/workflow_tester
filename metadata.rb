e30a1ba48c5c61f7a0811fbf2e8f54fa2e8bc212
Jp Robinson <dreamnite@penguinguru.com>
2017-05-29 18:49:16 -0400

    bump for changed_file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dreamnite@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures workflow_tester'
 long_description 'Installs/Configures workflow_tester'
-version '0.0.1'
+version '0.0.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/dreamnite/workflow_tester/issues'
